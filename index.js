87174db16012b8b5b43699b060916a8cd2a3d655
harshbhatt1380 <janmijaybhatt222@gmail.com>
2025-08-21 09:25:46 +0530

    Github upload of JS

---
+++ index.js
@@ -0,0 +1,21 @@
+//console.log(`New F1 fan`);
+//console.log(`Forgotten?`);
+//window.alert(`Do we need to remember all this?`)
+//document.getElementById("h1").textContent="Let's learn JavaScript shall we?";
+//document.getElementById("p1").textContent="I am learning JavaScript";
+
+let firstname = "Harsh";
+
+let x = 25; 
+let age = 2025-2002;
+let gpa = 7.2;
+let email = "janmijaybhatt222@gmail.com";
+console.log(x*5);
+console.log(`Hi Harsh you are ${age} years old`)
+console.log(`Your marks at college are : ${gpa}/10.0`)
+//Seeing data type of age 
+console.log(typeof age);
+console.log(typeof gpa);
+console.log(typeof firstname);
+console.log(email)
+//This is my comment
